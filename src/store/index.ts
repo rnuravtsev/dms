a5b5cb869414f7a763792373464d862e77a9659a
rnuravtsev <rnuravtsev@gmail.com>
2024-06-15 18:25:29 +0300

    feat(theme): adds theme store and events

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,19 @@ import { createEvent, createStore } from 'effector'
 
 import { SearchType } from '@components/Clinics/types'
 import { clinics as clinicsJSON } from '@shared/adapters/clinicsAdapter'
-import type { ClinicsType } from '@shared/types'
+import type { ClinicsType, ThemeType } from '@shared/types'
 
 export const updateSearchValue = createEvent<string>()
 export const updateSearchType = createEvent<SearchType>('')
 export const updateClinics = createEvent<ClinicsType>()
+export const updateTheme = createEvent<ThemeType>('')
 
 export const $searchValue = createStore<string>('')
 export const $searchType = createStore<SearchType>(SearchType.Address)
 export const $clinics = createStore<ClinicsType>(clinicsJSON)
+export const $theme = createStore<ThemeType>('light')
 
 $searchType.on<SearchType>(updateSearchType, (_, type) => type)
 $searchValue.on<string>(updateSearchValue, (_, value) => value)
 $clinics.on<ClinicsType>(updateClinics, (_, value) => value)
+$theme.on<ThemeType>(updateTheme, (_, value) => value)
